f726e6ce9d48d983b5aec4665580a66d17a6c6ae
Craig Miller <craigcmiller@gmail.com>
2017-03-05 19:25:28 +0000

    Initial commit

---
+++ CSShapefile/ShapefileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace CSShapefile
+{
+	/// <summary>
+	/// Shapefile reading support
+	/// http://www.esri.com/library/whitepapers/pdfs/shapefile.pdf
+	/// </summary>
+	public class ShapefileReader
+	{
+		private readonly Stream _stream;
+
+		public ShapefileReader(Stream stream)
+		{
+			_stream = stream;
+		}
+	}
+}
