c5f80b176ea60e92bb33be3084776d1ffaf81641
Craig Miller <craigcmiller@gmail.com>
2017-03-19 21:47:29 +0000

    ShapePoint

---
+++ CSShapefile/ShapePoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CSShapefile
+{
+	/// <summary>
+	/// Shape point
+	/// </summary>
+	public struct ShapePoint
+	{
+		public ShapePoint(double x, double y)
+		{
+			X = x;
+			Y = y;
+		}
+
+		public double X { get; }
+
+		public double Y { get; }
+
+		public override string ToString()
+		{
+			return string.Format("[ShapePoint: X={0}, Y={1}]", X, Y);
+		}
+	}
+}
